a015909d35cccc2af47ff3816fb29733c0583581
noukaza <amranenoureddin3@gmail.com>
2020-02-19 09:28:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ dist = "dist"
 
 def gestionFile(path, firstLine):
     folder = firstLine.replace('\n', '')
-    if not os.path.exists(dist+"/"+folder):
-        os.mkdir(dist+"/"+folder) 
-    splitpath = path.split('/')
+    folder = firstLine.replace(' ', '')
+    if not os.path.exists(dist+"\\"+folder):
+        os.mkdir(dist+"\\"+folder) 
+    splitpath = path.split('\\')
     name=  splitpath[len(splitpath) - 1]
-    copyfile(path, dist+"/"+folder+"/"+name)
+    pa = os.path.dirname(os.path.abspath(__file__))
+    pathSrc = os.path.join (pa,path)
+    pathDist = os.path.join(pa,dist,folder,name)
+    copyfile(pathSrc,pathDist)
 
 
-files = glob.glob(src+'/**/*.*')
-files.extend(glob.glob(src+'/*.*'))
+files = glob.glob(src+'\\**\\*.*')
+files.extend(glob.glob(src+'\\*.*'))
 
 for filepath in files :
-    file_name = filepath.split('/')
+    file_name = filepath.split('\\')
     file_name = file_name[1]
-    proto = file_name.split(':--:')[0]
+    proto = file_name.split('_--_')[0]
     # infile = open(filepath, 'r')
     gestionFile(filepath,proto)
 
